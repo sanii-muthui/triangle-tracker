4b13ef3b5fc6e7bd3aae15d9a1b338abac470f4e
sanii muthui <muthuisanii@gmail.com>
2019-06-02 19:13:56 +0300

    added inner.HTML

--- js/script.js
+++ js/script.js
@@ -1,23 +1,22 @@
-function formValidation(){
+function result(){
     var sideA=parseInt(document.getElementById('sideA').value);
     var sideB=parseInt(document.getElementById('sideB').value);
     var sideC=parseInt(document.getElementById('sideC').value);
     var sides=[sideA,sideB,sideC];
     if (sideA==sideB && sideB==sideC){
-    alert("Triangle is equilateral");
+    output.innerHTML=("Triangle is equilateral")
     }
     else if(sideA==sideB || sideA==sideC || sideB==sideC){
-    alert("Triangle is isosceles");
+    output.innerHTML=("Triangle is isosceles")
     }else if(sideA <= (sideB + sideC) || sideB <= (sideC + sideA) || sideC <= (sideA + sideB)){
-    alert("not a triangle");
+    output.innerHTML=("Not A Triangle")
     }else{
-    aletr("scalene");
+    output.innerHTML=("Triangle is Scalene")
     }
     }
-    return(formValidation());
-    function reset(){
+    /*function reset(){
       var sideA=parseInt(document.getElementById('sideC').value);
       var sideB=parseInt(document.getElementById('sideC').value);
       var sideC=parseInt(document.getElementById('sideC').value);
       var answer=document.getElementById('answer');
-    }
+    }*/
