22ecb415664de97870bf045b7f47195e4b648d35
sanii muthui <muthuisanii@gmail.com>
2019-06-03 09:45:59 +0300

    output changed to display.innerHTML

--- js/script.js
+++ js/script.js
@@ -4,19 +4,16 @@ function result(){
     var sideC=parseInt(document.getElementById('sideC').value);
     var sides=[sideA,sideB,sideC];
     if (sideA==sideB && sideB==sideC){
-    output.innerHTML=("Triangle is equilateral")
+    display.innerHTML=("Triangle is equilateral")
     }
     else if(sideA==sideB || sideA==sideC || sideB==sideC){
-    output.innerHTML=("Triangle is isosceles")
+    display.innerHTML=("Triangle is isosceles")
     }else if(sideA <= (sideB + sideC) || sideB <= (sideC + sideA) || sideC <= (sideA + sideB)){
-    output.innerHTML=("Not A Triangle")
+    display.innerHTML=("Not A Triangle")
     }else{
-    output.innerHTML=("Triangle is Scalene")
+    display.innerHTML=("Triangle is Scalene")
     }
     }
-    /*function reset(){
-      var sideA=parseInt(document.getElementById('sideC').value);
-      var sideB=parseInt(document.getElementById('sideC').value);
-      var sideC=parseInt(document.getElementById('sideC').value);
-      var answer=document.getElementById('answer');
-    }*/
+    function reset(){
+      location.reload();
+    }
